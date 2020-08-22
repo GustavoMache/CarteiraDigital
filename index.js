28e2b7af7f390c8400e764c54c673202411bb343
Gustavo <gamegu07@gmail.com>
2020-08-21 22:41:50 -0300

    feat: express instaled

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('olá, aobaaa!');
+const express = require('express');
+const server = express();
+
+server.get('/', function(request, response){
+    response.send('<h1>Aooobaa!</h1>');
+});
+
+
+
+
+server.listen(3333, function(){
+    console.log('to on!');
+});
