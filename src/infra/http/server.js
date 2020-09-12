f22f8c3e3fccd5182f8d5fbc8f3d6d0884170a09
Gustavo <gamegu07@gmail.com>
2020-09-11 21:34:23 -0300

    chore: husky

--- src/infra/http/server.js
+++ src/infra/http/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 const server = express();
 
 const Routes = require('./routes');
+
 server.use(express.json());
 server.use(Routes);
 
-
-server.listen(3333, function(){
-    console.log('to on!');
+server.listen(3333, () => {
+  /* eslint-disable-next-line */
+  console.log('to on!');
 });
