ad620c85a98afd9931c350fb4652647255a37765
Gustavo <gamegu07@gmail.com>
2020-09-11 22:10:05 -0300

    código atualizado 11-09-2020

--- src/modules/users/controllers/UserController.js
+++ src/modules/users/controllers/UserController.js
@@ -1,28 +1,25 @@
 const SignUpService = require('../service/SignUpService');
 
-class UserController
-{
-    async create(request, response)
-    {
-        const {name, email, password, password_confirm} = request.body;
-        
-        if(!name)
-            return response.json({message: 'name is required'});
-        if(!email)
-            return response.json({message: 'email is required'});
-        if(!password)
-            return response.json({message: 'password is required'});
-        if(!password_confirm)
-            return response.json({message: 'password_confirm is required'});
-        if(password !== password_confirm)
-            return response.json({message: 'password not match'});
-        
-        const user = await SignUpService.execute({
-            name, email, password
-        });
+class UserController {
+  async create(request, response) {
+    const { name, email, password, passwordconfirm } = request.body;
 
-        return response.json(user);
-    }
+    if (!name) return response.json({ message: 'name is required' });
+    if (!email) return response.json({ message: 'email is required' });
+    if (!password) return response.json({ message: 'password is required' });
+    if (!passwordconfirm)
+      return response.json({ message: 'password_confirm is required' });
+    if (password !== passwordconfirm)
+      return response.json({ message: 'password not match' });
+
+    const user = await SignUpService.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
 }
 
 module.exports = new UserController();
--- src/modules/users/service/SignUpService.js
+++ src/modules/users/service/SignUpService.js
@@ -1,20 +1,18 @@
 const UsersRepository = require('../repositories/UsersRepository');
 
-class SignUpService
-{
-    async execute(data)
-    {
-        const {name, email, password} = data;
+class SignUpService {
+  async execute(data) {
+    const { name, email, password } = data;
 
-        const emailAlreadyUsed = await UsersRepository.findByEmail(email);
+    const emailAlreadyUsed = await UsersRepository.findByEmail(email);
 
-        if(emailAlreadyUsed)
-            return {error: 'email not avaiable. Choise another!'};
+    if (emailAlreadyUsed)
+      return { error: 'email not avaiable. Choise another!' };
 
-        const user = await UsersRepository.add({name, email, password});
+    const user = await UsersRepository.add({ name, email, password });
 
-        return user;
-    }
+    return user;
+  }
 }
 
 module.exports = new SignUpService();
